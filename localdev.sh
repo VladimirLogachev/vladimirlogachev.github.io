e40118145d95b18ab2011ac9d26ad8e0ba7b5330
Vladimir Logachev <doit@keemail.me>
2019-12-23 19:09:41 +0700

    update action

--- localdev.sh
+++ localdev.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 rm -rf "./build" \
-&& mkdir "build" \
 && cp -r "./src/static" "./build" \
 && elm-live "src/Main.elm" \
-  --dir="./build/static" \
+  --dir="./build" \
   --open \
   --port=8000 \
   --pushstate \
   --start-page "index.html" \
   -- \
-  --output="./build/static/bundle.js"
+  --output="./build/bundle.js"
 
