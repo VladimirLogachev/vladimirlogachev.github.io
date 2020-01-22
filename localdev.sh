cc86d5d77e231affecba8569ebfcd638b84fe79d
Vladimir Logachev <doit@keemail.me>
2020-01-23 02:10:53 +0700

    Adjust Imagemagick webp settings

--- localdev.sh
+++ localdev.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 # Remove old build
-rm -rf "./build" 
+rm -rf "./build"
 
 # Copy static files
-cp -r "./src/static" "./build" 
+cp -r "./src/static" "./build"
 
 # Resize and optimize all raster images, which have width >200px
-for filename in $(find ./build/images -name '*.jpg' -or -name '*.png' -or -name '*.gif' -or -name '*.jpeg' -or -name '*.webp'); do convert "$filename" -resize '200x100000>' "$filename"; done
+for filename in $(find ./build/images -name '*.jpg' -or -name '*.png' -or -name '*.gif' -or -name '*.jpeg' -or -name '*.webp')
+do 
+  convert "$filename" -resize '400x100000>' -quality 80 -define webp:lossless=false -define webp:method=6 -define webp:partitions=3 -define webp:auto-filter=true "$filename"
+done
+
+# Show stats
+echo "Directory sizes in Kb:"
+du -sk src/static/images
+du -sk build/images
 
 # Run elm dev server
 elm-live "src/Main.elm" \
@@ -18,4 +26,3 @@ elm-live "src/Main.elm" \
   --start-page "index.html" \
   -- \
   --output="./build/bundle.js"
-
