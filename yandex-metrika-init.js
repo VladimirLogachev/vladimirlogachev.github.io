c3bc887368580b5a5897fabedbb79ba2ef4e332f
VladimirLogachev <VladimirLogachev@users.noreply.github.com>
2020-01-24 10:49:40 +0000

    Automated deployment: Fri Jan 24 10:49:40 UTC 2020 48f164b076432bf9fc451bfe724798799fe8840d

---
+++ yandex-metrika-init.js
@@ -0,0 +1,13 @@
+window.ym =
+  window.ym ||
+  function() {
+    (window.ym.a = window.ym.a || []).push(arguments);
+  };
+window.ym.l = 1 * new Date();
+ym(40142970, "init", {
+  clickmap: true,
+  trackLinks: true,
+  accurateTrackBounce: true,
+  webvisor: true,
+  trackHash: true
+});
